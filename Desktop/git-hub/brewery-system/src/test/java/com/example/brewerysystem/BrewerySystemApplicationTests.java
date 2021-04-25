6d536ef774e4835bd2abd58e44dc9f3d399d6235
Joe <jeric720@icloud.com>
2021-04-24 22:54:46 -0400

    initial commit

---
+++ Desktop/git-hub/brewery-system/src/test/java/com/example/brewerysystem/BrewerySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.brewerysystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrewerySystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
